1aa291c0f2a7e93d942bbb8c3d37bbad12e0a497
Anne738 <94481296+Anne738@users.noreply.github.com>
2022-09-14 01:09:34 +0300

    Add files via upload

---
+++ GeneratingRandomNumbers/ex1/ex1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+
+public class Randomseed
+{
+    public static void Main()
+    {
+        Random rnd = new Random(567);
+        for (int i = 0; i < 10; i++)
+        {
+            Console.Write("{0,3} ", rnd.Next(-100, 100));
+        }
+    }
+
+}
---
+++ GeneratingRandomNumbers/ex2/ex.cs
@@ -0,0 +1,23 @@
+﻿using System.Security.Cryptography;
+
+
+class RandomCrypto
+{
+    public static void Main()
+    {
+        var rnd = new RNGCryptoServiceProvider();
+        var rndNum = new byte[10];
+        rnd.GetBytes(rndNum);
+        for (int i = 0; i < 10; i++)
+        {
+            Console.WriteLine(rndNum[i]);
+        }
+        string cryptoKey = Convert.ToBase64String(rndNum);
+        Console.WriteLine(cryptoKey);
+    }
+}
+
+
+
+
+
