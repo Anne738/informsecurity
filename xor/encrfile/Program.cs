63182221e4f06ca03ace63ae27647fece5084b54
Anne738 <94481296+Anne738@users.noreply.github.com>
2022-10-26 22:20:31 +0300

    Add files via upload

---
+++ xor/encrfile/Program.cs
@@ -0,0 +1,51 @@
+﻿using System.Text;
+
+class Pass
+{
+    static void Main()
+    {
+        byte[] file_enc = File.ReadAllBytes(@"C:\KTC Net 2\encfile5.dat").ToArray();
+        string result = @"C:\KTC Net 2\result.txt";
+        string fraza = "Mit21";
+        byte[] b = Encoding.UTF8.GetBytes(fraza);
+        byte[] key = new byte[5];
+        var x = new XOR();
+
+        for (int i = 0; i < file_enc.Length - fraza.Length; i++)
+        {
+            for (int j = 0; j < 5; j++)
+            {
+                key[j] = file_enc[j + i];
+            }
+            var pass = x.Xor(key, b);
+            var text = x.Xor(file_enc, pass);
+            string dec = Encoding.UTF8.GetString(text);
+            if (dec.Contains(" Mit21 "))
+            {
+                File.AppendAllText(result, Encoding.UTF8.GetString(pass) + "\n" + dec);
+                Console.WriteLine("Done");
+            }
+        }
+    }
+
+    public class XOR
+    {
+        private byte[] GetKey(byte[] key, byte[] array)
+        {
+            byte[] secret = new byte[array.Length];
+            for (int i = 0; i < secret.Length; i++)
+            {
+                secret[i] = key[i % key.Length];
+            }
+            return secret;
+        }
+        public byte[] Xor(byte[] text, byte[] key)
+        {
+            for (int i = 0; i < text.Length; i++)
+            {
+                text[i] = (byte)(text[i] ^ GetKey(key, text)[i]);
+            }
+            return text;
+        }
+    }
+}
---
+++ xor/xor/Program.cs
@@ -0,0 +1,65 @@
+﻿using System.Text;
+
+
+
+class XOR
+{
+    public static void Main(string[] args)
+    {
+        byte[] readContent = File.ReadAllBytes(@"C:\KTC Net 2\mit.txt").ToArray();
+        Console.WriteLine(Encoding.UTF8.GetString(readContent));
+        Console.WriteLine("1 - encrypt file");
+        Console.WriteLine("2 - decrypt file");
+        Console.Write("Choose ");
+        int option = Convert.ToInt32(Console.ReadLine());
+        string path = @"C:\KTC Net 2\mit.txt";
+
+        switch (option)
+        {
+            case 1:
+                {
+                    var exor = new XOR_Program();
+                    Console.WriteLine("Write password");
+                    string keyword = Console.ReadLine();
+                    byte[] password = Encoding.UTF8.GetBytes(keyword);
+                    var encr = exor.XoR(readContent, password);
+                    Console.WriteLine("Done");
+                    File.WriteAllBytes(path + "file.dat", encr);
+                    break;
+                }
+            case 2:
+                {
+                    byte[] encryptContent = File.ReadAllBytes(path + "file.dat").ToArray();
+                    var dxor = new XOR_Program();
+                    Console.WriteLine("Write password");
+                    string keyword = Console.ReadLine();
+                    byte[] password = Encoding.UTF8.GetBytes(keyword);
+                    var decr = dxor.XoR(encryptContent, password);
+                    Console.WriteLine("Done");
+                    File.WriteAllBytes(path + "file.txt", decr);
+                    break;
+                }
+        }
+    }
+
+    public class XOR_Program
+    {
+        private byte[] GetKey(byte[] key, byte[] array)
+        {
+            byte[] secret = new byte[array.Length];
+            for (int i = 0; i < secret.Length; i++)
+            {
+                secret[i] = key[i % key.Length];
+            }
+            return secret;
+        }
+        public byte[] XoR(byte[] text, byte[] key)
+        {
+            for (int i = 0; i < text.Length; i++)
+            {
+                text[i] = (byte)(text[i] ^ GetKey(key, text)[i]);
+            }
+            return text;
+        }
+    }
+}
