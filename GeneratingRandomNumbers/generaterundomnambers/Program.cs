cecf35291c3b2cd6564b2dba30bed0fa8282e21d
Anne738 <94481296+Anne738@users.noreply.github.com>
2022-10-26 17:33:13 +0300

    Add files via upload

---
+++ GeneratingRandomNumbers/generaterundomnambers/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Security.Cryptography;
+class Rancryp
+{
+    public static void Main()
+    {
+        for (int i = 0; i < 10; i++)
+        {
+            Console.WriteLine(Convert.ToBase64String(Random.GeneratorRndNum(64)));
+        }
+    }
+}
+public class Random
+{
+    public static byte[] GeneratorRndNum(int bit)
+    {
+        using (var rnd = new RNGCryptoServiceProvider())
+        {
+            var rndNum = new byte[bit];
+            rnd.GetBytes(rndNum);
+            int Numb = BitConverter.ToInt32(rndNum, 0);
+            Console.WriteLine(Numb);
+            return rndNum;
+        }
+    }
+}
---
+++ GeneratingRandomNumbers/seed/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+public class Randomseed
+{
+    public static void Main()
+    {
+        int seed = 3222;
+        int seed1 = 3222;
+        int seed2 = 5555;
+        int amount = 5;
+        int min = 0;
+        int max = 100;
+        random(seed, amount, min, max);
+        random(seed1, amount, min, max);
+        random(seed2, amount, min, max);
+    }
+    public static void random(int seed, int amount, int min, int max)
+    {
+
+        Random rnd = new Random(seed);
+        Console.WriteLine("Numbers: ");
+        for (int i = 0; i < amount; i++)
+        {
+            Console.WriteLine(rnd.Next(min, max));
+        }
+    }
+}
